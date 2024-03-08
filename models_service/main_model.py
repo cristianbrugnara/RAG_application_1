2676a760793212024c044e02c580f8ea048d3c04
Cristian704 <120493260+Cristian704@users.noreply.github.com>
2024-03-08 23:30:35 +0100

    Added structure of metadata pipeline to main model

--- models_service/main_model.py
+++ models_service/main_model.py
@@ -5,7 +5,21 @@ from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
 from langchain.chat_models import ChatOpenAI
 import os
 from pinecone import Pinecone
-from resources.playground_secret_key import PINECONE_KEY,SECRET_KEY
+from resources.playground_secret_key import PINECONE_KEY, SECRET_KEY
+from typing import List, Dict
+
+# llama mettadata stuff
+from llama_index.llms.openai import OpenAI
+from llama_index.core.extractors import (
+    SummaryExtractor,
+    QuestionsAnsweredExtractor,
+    TitleExtractor,
+    KeywordExtractor,
+    BaseExtractor,
+)
+from llama_index.core.node_parser import TokenTextSplitter
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.ingestion import IngestionPipeline
 
 os.environ['PINECONE_API_KEY'] = PINECONE_KEY
 environment = os.environ.get('PINECONE_ENVIRONMENT')
@@ -26,6 +40,7 @@ class MainModel:
         __vector_store (Pinecone) : contains the vectorstore used to do similarity search on the index 
         __chat (ChatOpenAI)
     """
+
     @classmethod
     def __augment_prompt(cls, query: str, k: int):
         """Takes as input a user query and concatenate it with inherent documents taken from the vectorstore database, based on similarity
@@ -53,7 +68,7 @@ class MainModel:
         :param k: number of documents retrieved from the similarity search
         :return: source knowledge from the vectorstore database that relates to the user query
         """
-        results = cls.__vector_store.similarity_search(query=query, namespace=namespace,k=k)
+        results = cls.__vector_store.similarity_search(query=query, namespace=namespace, k=k)
         source_knowledge = '\n'.join([x.page_content for x in results])
         return source_knowledge
 
@@ -65,7 +80,7 @@ class MainModel:
         :return: return the model's response informed by the knowledge base retrieved from the index + user_query
         """
         prompt = [HumanMessage(
-            content=cls.__augment_prompt(user_query,1)
+            content=cls.__augment_prompt(user_query, 1)
         )]
         return cls.__chat_model.invoke(prompt).content
 
@@ -81,5 +96,37 @@ class MainModel:
     def remove_file(cls):
         pass
 
+    # metadata with llamaindex, maybe can be done with langchain
 
-print(MainModel.query('According to Michela Papandrea, what are the main steps of M.L. analysis'))
+    @classmethod
+    def generate_metadata(cls, filepaths: List[str], title: bool = True, summary: bool = False,
+                          qna: bool = False, keyword: bool = True) -> List[Dict]:
+
+        extractor = OpenAI(temperature=0.1, model="gpt-3.5-turbo", max_tokens=512)
+
+        splitter = TokenTextSplitter(separator=" ", chunk_size=512, chunk_overlap=128)
+
+        transformations = [splitter]
+        if title:
+            transformations.append(TitleExtractor(nodes=5, llm=extractor))
+        if qna:
+            transformations.append(QuestionsAnsweredExtractor(questions=3, llm=extractor))
+        if summary:
+            transformations.append(SummaryExtractor(llm=extractor))
+        if keyword:
+            transformations.append(KeywordExtractor(llm=extractor))
+
+        # obtain file text (probably in a different way)
+        docs = SimpleDirectoryReader(input_files=filepaths).load_data()
+
+        ing_pipeline = IngestionPipeline(transformations=transformations)
+        nodes = ing_pipeline.run(documents=docs)
+
+        result = [node.metadata for node in nodes]
+        return result  # need to decide how/where to get output
+
+
+if __name__ == '__main__':
+    # print(MainModel.query('According to Michela Papandrea, what are the main steps of M.L. analysis'))
+    # print(MainModel.generate_metadata(['../data/01_introduction_to_SL-4.pdf'])[0])
+    pass
